71e03f994ed2959ac8d0c404ed1e3587074290a2
marco <marco@exemple.com>
2022-12-15 18:51:59 +0100

    version_2

--- script1.sh
+++ script1.sh
@@ -5,3 +5,4 @@ echo " archivage"
 tar -cvzf /mnt/repertoire/archive.tar.gz  /home/*
 echo "demontage"
 umount -t ext4 /dev/sdd1
+#cela juste pour monter la partition lors du lancement du script et puis apres compresser le fichier 
